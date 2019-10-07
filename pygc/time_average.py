b2333b9d802ec9f5edba0b54e57221e82350329d
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-10-08 06:07:31 +0900

    Bug fix: gravitational potential of two-phase medium should not be masked out

--- pygc/time_average.py
+++ pygc/time_average.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-def dataset_tavg(s, ts, te, Twarm=2.0e4):
+def dataset_tavg(s, nums, Twarm=2.0e4, sum=False):
     """
     Function to time-average Datasets
 
@@ -14,10 +14,8 @@ def dataset_tavg(s, ts, te, Twarm=2.0e4):
     ----------
     s : LoadSimTIGRESSGC
         LoadSimTIGRESSGC instance to be analyzed.
-    ts : int
-        Start index.
-    te : int
-        End index.
+    nums : int
+        list of snapshot numbers
     Twarm : float
         A demarcation temperature for two-phase medium
 
@@ -28,29 +26,85 @@ def dataset_tavg(s, ts, te, Twarm=2.0e4):
     """
 
     cf = coolftn()
-    nums = np.arange(ts, te+1)
     fields = ['density','velocity','pressure','gravitational_potential']
 
     # load a first vtk
-    ds = s.load_vtk(num=ts)
+    ds = s.load_vtk(num=nums[0])
     dat = ds.get_field(fields, as_xarray=True)
-    add_derived_fields(dat, fields=['T','R'], in_place=True)
-    tmp = dat.where(dat.T < Twarm, other=1e-15) # two-phase medium
+    add_derived_fields(dat, fields=['T','Pturb'], in_place=True)
+    tmp = dat.where(dat.T < Twarm, other=1e-38) # two-phase medium
     dat = xr.concat([dat,tmp], pd.Index(['all', '2p'], name='phase'))
     dat = dat.drop('T')
-    add_derived_fields(dat, fields='Pturb', in_place=True)
+    # gravitational field exists even when the matter does not exist.
+    dat.gravitational_potential.loc[{'phase':'2p'}] = \
+            dat.gravitational_potential.loc[{'phase':'all'}]
+    add_derived_fields(dat, fields=['R','gz_sg'], in_place=True)
 
     # loop through vtks
     for num in nums[1:]:
         ds = s.load_vtk(num=num)
         tmp = ds.get_field(fields, as_xarray=True)
-        add_derived_fields(tmp, fields=['T','R'], in_place=True)
-        tmp2 = tmp.where(tmp.T < Twarm, other=1e-15)
+        add_derived_fields(tmp, fields=['T','Pturb'], in_place=True)
+        tmp2 = tmp.where(tmp.T < Twarm, other=1e-38)
         tmp = xr.concat([tmp,tmp2], pd.Index(['all', '2p'], name='phase'))
         tmp = tmp.drop('T')
-        add_derived_fields(tmp, fields='Pturb', in_place=True)
-        
+        # gravitational field exists even when the matter does not exist.
+        tmp.gravitational_potential.loc[{'phase':'2p'}] = \
+                tmp.gravitational_potential.loc[{'phase':'all'}]
+        add_derived_fields(tmp, fields=['R','gz_sg'], in_place=True)
+
         # combine
         dat += tmp
-    dat /= len(nums)
+    if not sum:
+        dat /= len(nums)
     return dat
+
+if __name__ == '__main__':
+    import argparse
+    from mpi4py import MPI
+    from pyathena.tigress_gc.load_sim_tigress_gc import LoadSimTIGRESSGC
+    from pyathena.util.split_container import split_container
+    import pickle
+
+
+    COMM = MPI.COMM_WORLD
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model', help='selected model')
+    parser.add_argument('start', type=int, help='start index')
+    parser.add_argument('end', type=int, help='end index')
+    parser.add_argument('-v', '--verbosity', action='count',
+                        help='increase output verbosity')
+    args = parser.parse_args()
+
+    if COMM.rank == 0:
+        if args.verbosity is not None:
+            if args.verbosity >= 2:
+                print("Running '{}'".format(__file__))
+            if args.verbosity >= 1:
+                print("selected model: {}".format(args.model))
+                print("generating time average between snapshot {} to {}"
+                        .format(args.start, args.end))
+    nums = np.arange(args.start,args.end+1)
+    if COMM.rank == 0:
+        nums = split_container(nums, COMM.size)
+    else:
+        nums = None
+    mynums = COMM.scatter(nums, root=0)
+    print('[rank, mysteps]:', COMM.rank, mynums)
+
+    s = LoadSimTIGRESSGC("/data/shmoon/TIGRESS-GC/{}".format(args.model))
+    dat = dataset_tavg(s, mynums, sum=True)
+
+    with open("{}.tavg.{}".format(args.model, COMM.rank), "wb") as handle:
+        pickle.dump(dat, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    COMM.Barrier()
+
+    if COMM.rank == 0:
+        for i in range(1, COMM.size):
+            dat += pickle.load(open("{}.tavg.{}".format(args.model, COMM.rank),
+                "rb"))
+        dat /= (args.end - args.start + 1)
+        with open("{}.tavg".format(args.model), "wb") as handle:
+            pickle.dump(dat, handle, protocol=pickle.HIGHEST_PROTOCOL)
