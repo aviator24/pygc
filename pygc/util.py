62c8a2f2dd3baa2eaadd9d1eeb6372f8225eb1ec
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-10-11 09:28:20 +0900

    feat: add function to count the number of SNe

--- pygc/util.py
+++ pygc/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import xarray as xr
 def wmean(arr, weights, dim):
     """
@@ -26,10 +27,51 @@ def wmean(arr, weights, dim):
 
 def mask_ring(dat, mf_crit=0.9, Rmax=180):
     """mask ring by applying density threshold and radius cut"""
-    rho, mf = _get_cummass(dat)
-    rhoth = rho[np.searchsorted(-mf, -mf_crit)]
-    mask = (dat.density > rhoth)&(dat.R < Rmax)
+    rhoth = 0
+    rho_mask = True
+    R_mask = True
+
+    if Rmax:
+        R_mask = dat.R < Rmax
+
+    if mf_crit:
+        if Rmax:
+            dat = dat.where(R_mask, other=0)
+        rho, mf = _get_cummass(dat)
+        rhoth = rho[np.searchsorted(-mf, -mf_crit)]
+        rho_mask = dat.density > rhoth
+
+    mask = rho_mask & R_mask
     return rhoth, mask
+
+def count_SNe(s, dat, ts, te, ncrit):
+    """Count the number of SNe exploded between time ts and te.
+
+    Parameters
+    ----------
+    s     : LoadSim instance
+    dat   : xr DataArray
+    ts    : start time in code unit
+    te    : end time in code unit
+    ncrit : SNe exploded at hydrogen number density below ncrit is not counted.
+    """
+    le1, le2 = s.domain['le'][0], s.domain['le'][1]
+    dx1, dx2 = s.domain['dx'][0], s.domain['dx'][1]
+    sn = s.read_sn()[['time','x1sn','x2sn','navg']]
+    sn = sn[(sn.time > ts)&(sn.time < te)&(sn.navg > ncrit)]
+    sn['i'] = np.floor((sn.x1sn-le1)/dx1).astype('int32')
+    sn['j'] = np.floor((sn.x2sn-le2)/dx2).astype('int32')
+    sn = sn.groupby(['j','i']).size()
+    # make Nx*Ny grid and project pSNe onto it
+    i = np.arange(s.domain['Nx'][0])
+    j = np.arange(s.domain['Nx'][1])
+    idx = pd.MultiIndex.from_product([j,i], names=['j','i'])
+    NSNe = pd.Series(np.zeros(s.domain['Nx'][0]*s.domain['Nx'][1]),
+            index=idx)
+    NSNe[sn.index] = sn
+    NSNe = NSNe.unstack().values
+    dat['NSNe'] = xr.DataArray(NSNe, dims=['y','x'],
+            coords=[dat.coords['y'], dat.coords['x']])
 
 def _Mabove(dat, rho_th):
     """Return total gas mass above threshold density rho_th."""
@@ -39,8 +81,8 @@ def _Mabove(dat, rho_th):
 
 def _get_cummass(dat):
     """Return n_th and M(n>n_th)"""
-    nbins = 100
-    thresholds = np.logspace(0, np.log10(dat.density.max().values[()]), nbins)
+    nbins = 50
+    thresholds = np.linspace(0, 100, nbins)
     cummass = np.zeros(nbins)
     Mtot = _Mabove(dat, 0)
     for i, rho_th in enumerate(thresholds):
