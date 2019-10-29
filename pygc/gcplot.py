29d1c7a1f0a08b5854b7bfd22252f4c85ce8c493
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-10-30 05:15:38 +0900

    merge serial and parallel plotting script

--- pygc/gcplot.py
+++ pygc/gcplot.py
@@ -5,62 +5,67 @@ Description | main plotting script for TIGRESS-GC
 Author      | Sanghyuk Moon
 =================================================
 """
+from pyathena.tigress_gc.load_sim_tigress_gc import LoadSimTIGRESSGC
+from pyathena.tigress_gc.plt_tigress_gc import plt_all
 import argparse
 import numpy as np
-from os import path as osp
-
-def draw_tigress_gc(indir, nums, all=None, projection=None, history=None):
-    """
-    =========================================================
-    Description | serial function to generate figures
-    Author      | Sanghyuk Moon
-    =========================================================
-    indir       | input simulation directory (indir/vtk; indir/starpar; etc.);
-    nums        | selected snapshots [start, end]
-    =========================================================
-    """
-    from pyathena.tigress_gc.load_sim_tigress_gc import LoadSimTIGRESSGC
-    from pyathena.tigress_gc.plt_tigress_gc import plt_proj_density, plt_all, plt_history
-    import time
-    import matplotlib.pyplot as plt
-
-    if all:
-        fsize = (32,18)
-    elif projection:
-        fsize = (22,12)
-
-    fig = plt.figure(figsize=fsize, dpi=60)
-
-    # Measure execution time
-    time0 = time.time()
-
-    s = LoadSimTIGRESSGC(indir, verbose=False)
-    
-    for num in nums:
-        dirname = osp.dirname(s.files['vtk'][0])
-        fvtk = osp.join(dirname, '{0:s}.{1:04d}.vtk'.format(s.problem_id, num))
-        if not osp.exists(fvtk):
-            continue
-        print(num, end=' ')
-        if all:
-            plt_all(s, num, fig, with_starpar=True)
-        elif projection:
-            plt_proj_density(s, num, fig)
-        fig.clf()
-    
-    print('')
-    print('################################################')
-    print('# Execution time [sec]: {:.1f}'.format(time.time()-time0))
-    print('################################################')
-    print('')
-
-    plt.close(fig)
+import matplotlib.pyplot as plt
+import os
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('indir', help='input simulation directory')
     parser.add_argument('start', type=int, help='start index')
     parser.add_argument('end', type=int, help='end index')
+    parser.add_argument('--mpi', action='store_true', help='enable mpi')
     args = parser.parse_args()
-    draw_tigress_gc(args.indir, np.arange(args.start,args.end+1),
-                    all=True, projection=False)
+
+    if args.mpi:
+        from mpi4py import MPI
+        from pyathena.util.split_container import split_container
+        COMM = MPI.COMM_WORLD
+        myrank = COMM.rank
+    else:
+        myrank = 0
+
+    nums = np.arange(args.start,args.end+1)
+    if args.mpi:
+        if myrank == 0:
+            nums = split_container(nums, COMM.size)
+        else:
+            nums = None
+        mynums = COMM.scatter(nums, root=0)
+    else:
+        mynums=nums
+    print('[rank, mysteps]:', myrank, mynums)
+
+    # Measure execution time
+    time0 = time.time()
+
+    # load simulation
+    s = LoadSimTIGRESSGC(args.indir, verbose=False)
+
+    # create figure instance
+    fsize = (32,18)
+    fig = plt.figure(figsize=fsize, dpi=60)
+
+    for num in mynums:
+        dirname = os.path.dirname(s.files['vtk'][0])
+        fvtk = os.path.join(dirname, '{0:s}.{1:04d}.vtk'.format(s.problem_id, num))
+        if not os.path.exists(fvtk):
+            continue
+        print(num, end=' ')
+        plt_all(s, num, fig, with_starpar=True)
+        fig.clf()
+
+    if args.mpi: 
+        COMM.barrier()
+    if myrank==0:
+        print('')
+        print('################################################')
+        print('# Execution time [sec]: {:.1f}'.format(time.time()-time0))
+        print('################################################')
+        print('')
+
+    plt.close(fig)
