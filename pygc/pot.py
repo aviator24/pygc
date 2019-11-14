fc2344c744f7f6b8dd59e7a65ee59784ba4b07f9
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-11-14 12:43:30 -0500

    refactoring potential models

--- pygc/pot.py
+++ pygc/pot.py
@@ -5,21 +5,61 @@ from astropy import constants as ac
 
 u = Units()
 
-def Mbul(r, r_b=120, rho_b=265):
-    """Bulge mass within radius r [pc]
-    return in Msun
-    """
-    return 4.*np.pi*r_b**3*rho_b*(np.log(r/r_b + np.sqrt(1.+r**2/r_b**2))
-            - r/r_b/np.sqrt(1.+r**2/r_b**2))
+class MHubble():
+    """Modified Hubble potential"""
+    def __init__(self, rb, rhob):
+        self.r_b = rb
+        self.rho_b = rhob
+    
+    def Menc(self, r):
+        """enclosed mass within radius r [pc]
+        return in Msun
+        """
+        M = 4.*np.pi*self.r_b**3*self.rho_b*(np.log(r/self.r_b
+            + np.sqrt(1.+r**2/self.r_b**2))
+            - r/self.r_b/np.sqrt(1.+r**2/self.r_b**2))
+        return M
+    
+    def vcirc(self, R, z):
+        """Circular velocity at radius (R,z) [pc]
+        return in km/s
+        """
+        r = np.sqrt(R**2+z**2)
+        vsq = 4*np.pi*ac.G.to('km2 s-2 pc Msun-1').value\
+                *self.rho_b*self.r_b**2*(self.r_b/r*np.log(r/self.r_b
+                +np.sqrt(1.+r**2/self.r_b**2))
+                -1./np.sqrt(1.+r**2/self.r_b**2))
+        return np.sqrt(vsq)*R/r
 
-def vcirc(r, r_b=120, rho_b=265):
-    """Circular velocity at radius r [pc]
-    return in km/s
-    """
-    vsq = 4*np.pi*ac.G.to('km^2 s^-2 pc Msun^-1').value*rho_b*r_b**2*(
-            r_b/r*np.log(r/r_b + np.sqrt(1.+r**2/r_b**2))
-            - 1./np.sqrt(1.+r**2/r_b**2))
-    return np.sqrt(vsq)
+    def rho(self, r):
+        """stellar density at radius r [pc]
+        return in Msun/pc^3
+        """
+        return self.rho_b / (1.+r**2/self.r_b**2)**1.5
+
+    def rho_eff(self, R, z):
+        """Effective bulge stellar density at (R,z) [pc],
+        defined as g_z(R,z) = -4 pi G rho_eff(R,z) z (OML10)
+        return in Msun/pc^3
+        """
+        r = np.sqrt(R**2 + z**2)
+        return self.rho_b*(self.r_b/r)**2*(self.r_b/r*np.log(r/self.r_b
+            + np.sqrt(1.+r**2/self.r_b**2)) - 1./np.sqrt(1.+r**2/self.r_b**2))
+
+    def gz(self, R, z):
+        """Vertical gravitational acceleration at (R,z) [pc]
+        return in km/s/Myr
+        """
+        gz = -4*np.pi*ac.G.to("km s^-1 Myr^-1 Msun^-1 pc^2").value\
+                *self.rho_eff(R, z)*z
+        return gz
+
+    def gz_linear(self, R, z):
+        """Linearly approximated vertical gravitational acceleration at (R,z) [pc]
+        return in km/s/Myr
+        """
+        return -4*np.pi*ac.G.to("km s^-1 Myr^-1 Msun^-1 pc^2").value\
+                *self.rho_eff(R,0)*z
 
 def vcirc_KE17(R):
     """Kim & Elmegreen (2017) rotation curve (R is given in pc)
@@ -27,34 +67,4 @@ def vcirc_KE17(R):
     """
     return 215 + 95*np.tanh((R-70)/60) - 50*np.log10(R) + 1.5*(np.log10(R))**3
 
-def rhobul(r, r_b=120, rho_b=265):
-    """Bulge stellar density at radius r [pc]
-    return in Msun/pc^3
-    """
-    return rho_b / (1.+r**2/r_b**2)**1.5
 
-def rhobul_eff(R, z, r_b=120, rho_b=265):
-    """Effective bulge stellar density at (R,z) [pc],
-    defined as g_z(R,z) = -4 pi G rho_eff(R,z) z (OML10)
-    return in Msun/pc^3
-    """
-    r = np.sqrt(R**2 + z**2)
-    return rho_b*(r_b/r)**2*(r_b/r*np.log(r/r_b + np.sqrt(1.+r**2/r_b**2))
-            - 1./np.sqrt(1.+r**2/r_b**2))
-
-def gz_ext(R, z, r_b=120, rho_b=265, TIGRESS_unit=False):
-    """Vertical gravitational acceleration at (R,z) [pc]
-    return in km/s/Myr
-    """
-    gz = -4*np.pi*ac.G.to("km s^-1 Myr^-1 Msun^-1 pc^2").value\
-            *rhobul_eff(R,z,r_b=r_b,rho_b=rho_b)*z
-    if TIGRESS_unit:
-        gz *= u.Myr
-    return gz
-
-def gz_ext_linear(R, z, r_b=120, rho_b=265):
-    """Linearly approximated vertical gravitational acceleration at (R,z) [pc]
-    return in km/s/Myr
-    """
-    return -4*np.pi*ac.G.to("km s^-1 Myr^-1 Msun^-1 pc^2").value\
-            *rhobul_eff(R,0,r_b=r_b,rho_b=rho_b)*z
