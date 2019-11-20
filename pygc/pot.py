2983aa6d88684e10e63714be308cf235e9a90b73
Sanghyuk Moon <moon@astro.snu.ac.kr>
2019-11-21 06:16:39 +0900

    make base class for potential models

--- pygc/pot.py
+++ pygc/pot.py
@@ -1,18 +1,22 @@
-"""
-Collection of potentials
-unit system : [M] = Msun, [V] = km/s, [L] = pc
-"""
 from pyathena.util.units import Units
 import numpy as np
 from astropy import units as au
 from astropy import constants as ac
 
-class rigid():
+class pot():
+    """
+    Base class for potential models
+    unit system : [M] = Msun, [V] = km/s, [L] = pc
+    """
+    def __init__(self):
+        self.G = ac.G.to('Msun-1 pc km2 s-2').value
+
+class rigid(pot):
     """rigid body rotation"""
     def __init__(self, Omg):
+        super().__init__()
         self.Omg = Omg
         self.Omg2 = Omg**2
-        self.G = ac.G.to('Msun-1 pc km2 s-2').value
         self.rho = 3*self.Omg2/(4*np.pi*self.G)
     def Menc(self, x, y, z):
         r3 = (x**2+y**2+z**2)**1.5
@@ -27,8 +31,9 @@ class rigid():
     def gz(self, x, y, z):
         return -z*self.Omg2
 
-class Ferrers():
+class Ferrers(pot):
     """n=1 Ferrers bar"""
+    # TODO 3D generalization
     def __init__(self, rhobar, a, b):
         self.rhobar = rhobar
         self.a = a
@@ -42,7 +47,6 @@ class Ferrers():
         self.W11 = (self.W01-self.W10)/(a**2*e**2)
         self.W20 = 2./3.*(1./a**5/(1.-e**2) - self.W11)
         self.W02 = 0.25*(2./a**5/(1.-e**2)**2 - self.W11)
-        self.G = ac.G.to('Msun-1 pc km2 s-2').value
     def rho(self, x, y):
         g2 = (y/self.a)**2 + (x/self.b)**2
         den = np.zeros(g2.shape)
@@ -59,71 +63,35 @@ class Ferrers():
         coeff = 0.5*np.pi*self.G*self.a*self.b**2*self.rhobar
         return coeff*(-4.*self.W10*y + 4.*self.W20*y**3 + 4.*self.W11*x**2*y)
 
-class MHubble():
+class MHubble(pot):
     """Modified Hubble potential"""
     def __init__(self, rb, rhob):
         self.r_b = rb
         self.rho_b = rhob
-        self.G = ac.G.to('Msun-1 pc km2 s-2').value
-    
     def Menc(self, r):
-        """enclosed mass within radius r [pc]
-        return in Msun
-        """
         M = 4.*np.pi*self.r_b**3*self.rho_b*(np.log(r/self.r_b
             + np.sqrt(1.+r**2/self.r_b**2))
             - r/self.r_b/np.sqrt(1.+r**2/self.r_b**2))
         return M
-    
     def vcirc(self, R, z):
-        """Circular velocity at radius (R,z) [pc]
-        return in km/s
-        """
         r = np.sqrt(R**2+z**2)
-        vsq = 4*np.pi*ac.G.to('km2 s-2 pc Msun-1').value\
-                *self.rho_b*self.r_b**2*(self.r_b/r*np.log(r/self.r_b
-                +np.sqrt(1.+r**2/self.r_b**2))
-                -1./np.sqrt(1.+r**2/self.r_b**2))
+        vsq = 4*np.pi*self.G*self.rho_b*self.r_b**2*(self.r_b/r*np.log(
+            r/self.r_b+np.sqrt(1.+r**2/self.r_b**2))
+            -1./np.sqrt(1.+r**2/self.r_b**2))
         return np.sqrt(vsq)*R/r
-
     def rho(self, r):
-        """stellar density at radius r [pc]
-        return in Msun/pc^3
-        """
         return self.rho_b / (1.+r**2/self.r_b**2)**1.5
-
-    def rho_eff(self, R, z):
-        """Effective bulge stellar density at (R,z) [pc],
-        defined as g_z(R,z) = -4 pi G rho_eff(R,z) z (OML10)
-        return in Msun/pc^3
-        """
-        r = np.sqrt(R**2 + z**2)
-        return self.rho_b*(self.r_b/r)**2*(self.r_b/r*np.log(r/self.r_b
-            + np.sqrt(1.+r**2/self.r_b**2)) - 1./np.sqrt(1.+r**2/self.r_b**2))
-
     def gx(self, x, y, z):
         r = np.sqrt(x**2+y**2+z**2)
         return -self.G*self.Menc(r)*x/r**3
-
     def gy(self, x, y, z):
         r = np.sqrt(x**2+y**2+z**2)
         return -self.G*self.Menc(r)*y/r**3
-
     def gz(self, x, y, z):
         r = np.sqrt(x**2+y**2+z**2)
         return -self.G*self.Menc(r)*z/r**3
-
-    def gz_linear(self, R, z):
-        """Linearly approximated vertical gravitational acceleration at (R,z) [pc]
-        return in km/s/Myr
-        """
-        return -4*np.pi*ac.G.to("km s^-1 Myr^-1 Msun^-1 pc^2").value\
-                *self.rho_eff(R,0)*z
-
 def vcirc_KE17(R):
     """Kim & Elmegreen (2017) rotation curve (R is given in pc)
     return in km/s
     """
     return 215 + 95*np.tanh((R-70)/60) - 50*np.log10(R) + 1.5*(np.log10(R))**3
-
-
