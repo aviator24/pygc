42ec7705800c3819221d582d7934411fa1f58a85
Sanghyuk Moon <smoon.richard@gmail.com>
2020-06-22 11:33:19 +0900

    add small ring parameters

--- pygc/mdot.py
+++ pygc/mdot.py
@@ -11,7 +11,7 @@ from pygc.pot import MHubble, Log, Plummer
 from pyathena.util.units import Units
 from scipy.optimize import bisect
 
-target_mdot = 0.5
+target_mdot = 2
 
 def Mdot(iflw_d0):
     Mdot = 0
@@ -26,16 +26,25 @@ def Mdot(iflw_d0):
 
 if __name__ == '__main__':
     u=Units()
-    dx = 16
+    dx = 8
     hdx = dx>>1
     iflw_mu=np.cos(10*au.deg)
     Omega_0 = 0.036*u.Myr
-    Rring = 600
+
+###### Small ring #####
+#    Rring =150
+#    iflw_b=80
+#    iflw_w=50
+#    iflw_h=50
+#    y0 = -256-hdx
+#    bul = MHubble(250, 50)
+#    BH = Plummer(1.4e8, 20)
+###### Large ring #####
+    Rring =600
     iflw_b=320
     iflw_w=200
     iflw_h=200
     y0 = -1024-hdx
-
     bul = MHubble(250, 50)
     BH = Plummer(1.4e8, 20)
 
