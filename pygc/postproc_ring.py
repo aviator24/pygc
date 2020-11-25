3802358fe6ea1f574df549db437da1cbccb6998e
Sanghyuk Moon <moon@astro.snu.ac.kr>
2020-11-24 21:02:58 -0500

    refactor ring average

--- pygc/postproc_ring.py
+++ pygc/postproc_ring.py
@@ -5,11 +5,11 @@ Description | Read snapshot, delineate ring, compute various quantities
 Author      | Sanghyuk Moon
 =======================================================================
 """
-from pygc.util import add_derived_fields, grid_msp
-from pygc.pot import MHubble, Plummer
-from pygc.ring import mask_ring_by_mass, _get_area
+
+from pygc.util import add_derived_fields
+from pygc.ring import mask_ring_by_mass, ring_avg
 from pyathena.tigress_gc.load_sim_tigress_gc import LoadSimTIGRESSGC
-from pyathena.io.read_vtk import read_vtk
+import pickle
 import argparse
 import numpy as np
 import os
@@ -23,17 +23,9 @@ if __name__ == '__main__':
     parser.add_argument('end', type=int, help='end index')
     parser.add_argument('--outdir', default=None, help='output directory')
     parser.add_argument('--mpi', action='store_true', help='enable mpi')
-    parser.add_argument('--twophase', action='store_true')
-    parser.add_argument('--mf_crit', type=float, help='mass cut')
+    parser.add_argument('--mf_crit', default=0.9, type=float, help='mass cut')
     parser.add_argument('--Rmax', type=float, help='radius cut')
-    parser.add_argument('--r0', type=float, default=250., help='bulge central radius')
-    parser.add_argument('--rho0', type=float, default=50., help='bulge central density')
-    parser.add_argument('--Mc', type=float, default=1.4e8, help='BH mass')
-    parser.add_argument('--rc', type=float, default=20., help='BH softning radius')
     args = parser.parse_args()
-
-    bul = MHubble(args.r0, args.rho0)
-    BH = Plummer(args.Mc, args.rc)
 
     if args.mpi:
         from mpi4py import MPI
@@ -51,11 +43,6 @@ if __name__ == '__main__':
         os.mkdir(outdir)
 
     fname = 'gc'
-    if args.twophase:
-        fname = fname+'.2p'
-    if args.mf_crit:
-        fname = fname+'.mcut{}'.format(args.mf_crit)
-
     nums = np.arange(args.start,args.end+1)
     if args.mpi:
         if myrank == 0:
@@ -69,93 +56,10 @@ if __name__ == '__main__':
 
     # load simulation
     s = LoadSimTIGRESSGC(args.indir)
-    dz = s.domain['dx'][2]
+    dat_tavg = pickle.load(open(args.indir+'/postproc_tavg/tavg.pkl','rb'))
+    add_derived_fields(dat_tavg, 'surf')
+    surf_th, mask = mask_ring_by_mass(dat_tavg, mf_crit=args.mf_crit, Rmax=args.Rmax)
 
     for num in mynums:
-        ds = s.load_vtk(num)
-        sp = s.load_starpar_vtk(num)
-        if sp.shape[1]==0:
-            print("no star particles are found")
-            flag_sp = False
-        else:
-            flag_sp = True
-        t = ds.domain['time']
-        dat = ds.get_field(['density','velocity','pressure',
-            'gravitational_potential'], as_xarray=True)
-        dat = dat.drop(['velocity1','velocity2'])
-
-        if args.twophase:
-            add_derived_fields(dat, ['T','gz_sg'])
-            gz_sg = dat.gz_sg
-            # select two-phase gas
-            dat = dat.where(dat.T < Twarm)
-            dat = dat.drop('T')
-            dat['gz_sg'] = gz_sg
-        else:
-            add_derived_fields(dat, ['gz_sg'])
-        dat = dat.drop('gravitational_potential')
-
-        # seperate individual contributions to the gravitational field.
-        ds = read_vtk('{}/postproc_gravity/gc.{:04d}.vtk'.format(s.basedir, num))
-        Phigas = ds.get_field('Phi').Phi
-        phir = Phigas.shift(z=-1)
-        phil = Phigas.shift(z=1)
-        phir.loc[{'z':phir.z[-1]}] = 3*phir.isel(z=-2) - 3*phir.isel(z=-3) + phir.isel(z=-4)
-        phil.loc[{'z':phir.z[0]}] = 3*phil.isel(z=1) - 3*phil.isel(z=2) + phil.isel(z=3)
-        gz_gas = (phil-phir)/(2*dz)
-        dat['gz_starpar'] = dat.gz_sg - gz_gas # order is important!
-        dat['gz_gas'] = gz_gas # order is important!
-        add_derived_fields(dat, 'R')
-        dat['gz_ext'] = bul.gz(dat.x, dat.y, dat.z).T + BH.gz(dat.x, dat.y, dat.z).T
-
-        # add derived fields
-        Pgrav_gas = -(dat.density*dat.gz_gas*dz).where(dat.z>0).sum(dim='z')
-        Pgrav_starpar = -(dat.density*dat.gz_starpar*dz).where(dat.z>0).sum(dim='z')
-        Pgrav_ext = -(dat.density*dat.gz_ext*dz).where(dat.z>0).sum(dim='z')
-
-        dat = dat.drop(['gz_sg', 'gz_starpar', 'gz_gas', 'gz_ext'])
-        add_derived_fields(dat, ['surf','H','Pturb'])
-        dat = dat.drop('velocity3')
-
-        dat['Pth_mid'] = dat.pressure.interp(z=0)
-        dat['Pturb_mid'] = dat.Pturb.interp(z=0)
-        dat['n0'] = dat.density.interp(z=0)
-        dat['Ptot_top'] = dat.Pturb.isel(z=-1)+dat.pressure.isel(z=-1)
-
-        if args.mf_crit:
-            # delineate the ring by applying a mass cut
-            surf_th, mask = mask_ring_by_mass(dat, mf_crit=args.mf_crit,
-                    Rmax=args.Rmax)
-        elif args.twophase:
-            mask = dat.Pturb_mid > 0
-        else:
-            mask = True
-        area = _get_area(dat.where(mask))
-
-        surf = dat.surf.where(mask).mean().values[()]
-        if flag_sp:
-            msp = grid_msp(s, num, 0, 1e10)
-            surfstar = msp.where(mask).sum().values[()]/area
-            agebin = 10/s.u.Myr
-            msp = grid_msp(s, num, 0, agebin)
-            surfsfr = msp.where(mask).sum().values[()]/area/agebin
-        else:
-            surfstar = 0
-            surfsfr = 0
-        n0 = dat.n0.where(mask).mean().values[()]
-        H = dat.H.where(mask).mean().values[()]
-        if flag_sp:
-            Hs = np.sqrt(0.5*(sp.mass*sp.x3**2).sum()/sp.mass.sum())
-        else:
-            Hs = 0
-
-        Pgrav_gas = Pgrav_gas.where(mask).mean().values[()]
-        Pgrav_starpar = Pgrav_starpar.where(mask).mean().values[()]
-        Pgrav_ext = Pgrav_ext.where(mask).mean().values[()]
-        Pturb = dat.Pturb_mid.where(mask).mean().values[()]
-        Pth = dat.Pth_mid.where(mask).mean().values[()]
-        Ptot_top = dat.Ptot_top.where(mask).mean().values[()]
-
-        np.savetxt("{}/{}.{:04d}.txt".format(outdir,fname,num),
-            [t, surf, surfstar, surfsfr, n0, H, Hs,
-            Pgrav_gas, Pgrav_starpar, Pgrav_ext, Pturb, Pth, Ptot_top, area])
+        ds = ring_avg(s, num, mask)
+        np.savetxt("{}/{}.{:04d}.txt".format(outdir,fname,num), ds)
